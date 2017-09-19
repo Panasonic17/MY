ff0514fc33166203a9c1b68d2694163de98a745d
oleksandr_shainoga <oleksandr_shainoga@epam.com>
2017-09-19 16:29:30 +0300

    docker-compoce add

---
+++ Self/docker 2 socets java/2 SOCETS/byild.sh
@@ -0,0 +1,4 @@
+cd client
+docker build -t socetclient .
+cd ../server
+docker build -t socetserver .
--- Self/test/src/main/java/Main.java
+++ Self/test/src/main/java/Main.java
@@ -11,15 +11,18 @@ public class Main {
     }
     public static void main(String[] args) throws Exception {
 
-        ArrayList<String> a = new ArrayList<String>();
-        a.add("1");
-        a.add("2");
-        a.add("3");
-        a.add("4");
-        a.add("5");
-        a.add("6");
-        a.add("7");
-        a.stream().peek(a1->qwe(a1)).forEach(System.out::println);
+//        ArrayList<String> a = new ArrayList<String>();
+//        a.add("1");
+//        a.add("2");
+//        a.add("3");
+//        a.add("4");
+//        a.add("5");
+//        a.add("6");
+//        a.add("7");
+//        a.stream().peek(a1->qwe(a1)).forEach(System.out::println);
+        String query="\"{\"\"query\"\": { \"\"match\"\": { \"\"user\"\": \"\"user<random_number>\"\" } }}\"";
+        String qe=query.replace("<random_number>",(int)(Math.random()*100.0)+"");
+        System.out.println(qe);
     }
 
 
